829fbe6aeeb61d00c110b59967aaeaca6769ebb2
Sergio Morales <chemoral87@hotmail.com>
2023-03-09 00:22:15 -0600

    post loader config

--- nuxt.config.js
+++ nuxt.config.js
@@ -209,8 +209,22 @@ export default {
       chunk: ({ isDev }) => (isDev ? '[name].js' : '[id].[contenthash].js')
     },
     postcss: {
-      plugins: {
-        'postcss-custom-properties': false
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      postcssOptions: {
+        plugins: {
+          // Disable a plugin by passing false as value
+          'postcss-url': false
+          // 'postcss-nested': {},
+          // 'postcss-responsive-type': {},
+          // 'postcss-hexrgba': {}
+        }
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
+        }
       }
     }
   }
