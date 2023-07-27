293a3d1d102264c6ac897e38ddaf1c33e922dbbc
Sergio Morales <chemoral87@hotmail.com>
2023-07-27 12:49:14 -0600

    cahceBusto

--- pages/church-service-rol/index.vue
+++ pages/church-service-rol/index.vue
@@ -105,7 +105,7 @@ export default {
     async exportImg() {
       domtoimage
         .toPng(this.captureElement, {
-          cacheBust: false
+          cacheBust: treu
           // height: this.captureElement.offsetHeight * 2,
           // width: this.captureElement.offsetWidth * 2,
           // style: {
