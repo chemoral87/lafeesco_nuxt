78ce99172162b226ec46c7705c636b5aadd70e82
Sergio Morales <chemoral87@hotmail.com>
2023-07-29 17:02:14 -0600

    v12

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -7,7 +7,7 @@
           {{ user.second_last_name }}
         </div>
         <div class="text-h6">Email: {{ user.email }}</div>
-        v.0.0.11
+        v.0.0.12
       </v-col>
       <v-col>
         <v-btn color="primary">Editar Nombre</v-btn>
