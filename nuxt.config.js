0132c5f845b67770dadc594b0f77809ff790dfa7
Sergio Morales <chemoral87@hotmail.com>
2023-09-04 16:38:47 -0600

    non fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    './plugins/qrcode-reader',
+    
     './plugins/mixins/user.js',
     './plugins/mixins/utils.js',
     './plugins/mixins/validation.js',
