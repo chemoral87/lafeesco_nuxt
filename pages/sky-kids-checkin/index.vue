62276e1c56d6f3e6c56e0cd3c8fcac1c18308b1a
Sergio Morales <chemoral87@hotmail.com>
2023-09-04 18:14:27 -0600

    implement QR reader

--- pages/sky-kids-checkin/index.vue
+++ pages/sky-kids-checkin/index.vue
@@ -1,24 +1,38 @@
 <template>
   <div>
-    <vue-qrcode-reader @decode="onDecode" />
-    <p v-if="decodedText">{{ decodedText }}</p>
+    <div id="reader"></div>
+    <div>{{ content }}</div>
   </div>
 </template>
 <script>
+import { Html5Qrcode } from "html5-qrcode";
+
 export default {
-  props: {},
   data() {
     return {
-      decodedText: null,
+      content: "",
     };
   },
-  methods: {
-    onDecode(decodedText) {
-      this.decodedText = decodedText;
-    },
-  },
   mounted() {
-    let me = this;
+    const html5QrcodeScanner = new Html5Qrcode("reader");
+    html5QrcodeScanner
+      .start(
+        { facingMode: "environment" },
+        {
+          fps: 10,
+          qrbox: { width: 250, height: 250 },
+        },
+        (qrCodeMessage) => {
+          console.log(`QR Code scanned! qrCodeMessage: ${qrCodeMessage}`);
+          this.content = qrCodeMessage;
+        },
+        (errorMessage) => {
+          console.log(`QR Code scan error, errorMessage: ${errorMessage}`);
+        }
+      )
+      .catch((err) => {
+        console.log(`Unable to start scanning, error: ${err}`);
+      });
   },
 };
 </script>
